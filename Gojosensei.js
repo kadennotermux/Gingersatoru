068b8f368cbe17c42bf32262138d83ae2fb558ef
kadennotermux <66335850+kadennotermux@users.noreply.github.com>
2022-06-26 18:43:35 +0300

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -2104,11 +2104,11 @@ break
             break
 	    case 'ytmp3': case 'getmusic': case 'ytaudio': {
                 let { yta } = require('./lib/y2mate')
-                if (!text) return reply(`Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 320kbps`)
+                if (!text) return reply(`Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 128kbps`)
                 let quality = args[1] ? args[1] : '320kbps'
                 let media = await yta(text, quality)
                 if (media.filesize >= 999999) return reply('File Over Limit '+util.format(media))
-                GojoMdNx.sendImage(m.chat, media.thumb, `⛷ Title : ${media.title}\n⛷ File Size : ${media.filesizeF}\n⛷ Url : ${isUrl(text)}\n⛷ Ext : MP3\n⛷ Resolution : ${args[1] || '320kbps'}`, m)
+                GojoMdNx.sendImage(m.chat, media.thumb, `⭓ Title : ${media.title}\n⭓ File Size : ${media.filesizeF}\n⭓ Url : ${isUrl(text)}\n⭓ Ext : MP3\n⭓ Resolution : ${args[1] || '320kbps'}`, m)
                 GojoMdNx.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'audio/mpeg', fileName: `${media.title}.mp3` }, { quoted: m })
             }
             break
